02934fdf91a2970654f7881ea1234d54d683835a
Kadek Winda Dwiastini <winda1996@hotmail.com>
2019-05-28 09:37:31 +0800

    add viper folder group

---
+++ goOSC/View/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  goOSC
+//
+//  Created by Bootcamp on 5/28/19.
+//  Copyright © 2019 Swift Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
