10388bdb11c7a7f71f6066240aa55f4831cbc63c
reni <reni_k901@mitrais.com>
2019-05-28 12:01:33 +0800

    add alamofire to the project

---
+++ goOSC/Login/Entity/Customer.swift
@@ -0,0 +1,19 @@
+//
+//  Customer.swift
+//  goOSC
+//
+//  Created by Bootcamp on 28/05/19.
+//  Copyright © 2019 Swift Bootcamp. All rights reserved.
+//
+
+import Foundation
+
+struct Customer {
+    var name: String
+    var email: String
+    
+    init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+}
---
+++ goOSC/Login/View/Login.swift
@@ -0,0 +1,26 @@
+//
+//  Login.swift
+//  goOSC
+//
+//  Created by Bootcamp on 28/05/19.
+//  Copyright © 2019 Swift Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func redirectToForgotPassword(_ sender: Any) {
+    }
+    
+    @IBAction func loginWithSosMed(_ sender: Any) {
+    }
+    
+    @IBAction func moveToRegister(_ sender: Any) {
+    }
+    
+    @IBAction func doLogin(_ sender: Any) {
+    }
+}
