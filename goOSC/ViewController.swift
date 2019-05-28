848afa3388cdc954b6099286da6ebd6b499189c7
Kadek Winda Dwiastini <winda1996@hotmail.com>
2019-05-28 09:10:25 +0800

    initial commit

---
+++ goOSC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goOSC
+//
+//  Created by Bootcamp on 5/28/19.
+//  Copyright © 2019 Swift Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
