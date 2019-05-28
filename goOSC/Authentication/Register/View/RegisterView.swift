d1f82c17c73ac66408a73bfdb45d4a19e3f02832
Kadek Winda Dwiastini <winda1996@hotmail.com>
2019-05-28 14:41:15 +0800

    refactor project

--- goOSC/Authentication/Register/View/RegisterView.swift
+++ goOSC/Authentication/Register/View/RegisterView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginView: UIViewController {
+class RegisterView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- goOSC/Authentication/Register/Wireframe/RegisterWireframe.swift
+++ goOSC/Authentication/Register/Wireframe/RegisterWireframe.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class LoginWireframe: UIViewController {
+class RegisterWireframe: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
