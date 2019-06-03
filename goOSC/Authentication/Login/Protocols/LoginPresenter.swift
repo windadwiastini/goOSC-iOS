e959ca68cbf9c17dac915b452b0e2fcde8835d1b
reni <reni_k901@mitrais.com>
2019-06-03 13:34:45 +0800

    change behaviour of show alert

--- goOSC/Authentication/Login/Protocols/LoginPresenter.swift
+++ goOSC/Authentication/Login/Protocols/LoginPresenter.swift
@@ -25,6 +25,5 @@ class LoginPresenter: LoginPresenterProtocol {
 extension LoginPresenter: LoginOutputInteractorProtocol {
     func response(_ resp: Customer.Response ) {
         view?.openAlert("Login", resp.message)
-        wireFrame?.routeToRegister(from: view as! UIViewController)
     }
 }
