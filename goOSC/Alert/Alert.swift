8e22ed715430b46e7db8580a37870cbe01f45c50
reni <reni_k901@mitrais.com>
2019-06-28 11:36:15 +0800

    add identifier and value over the alert

--- goOSC/Alert/Alert.swift
+++ goOSC/Alert/Alert.swift
@@ -12,12 +12,16 @@ class Alert {
     func informationAlert(title titleData: String, message content: String, ui ref: UIViewController){
         print("Alert class")
         let alert = UIAlertController(title: titleData, message: content, preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "informationAlert"
+        alert.view.accessibilityValue = "informationAlert"
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         ref.present(alert, animated: true)
     }
     
     func alertWithClosure(title titleData: String, message content: String, ui ref: UIViewController) {
         let alert = UIAlertController(title: titleData, message: content, preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "closureAlert"
+        alert.view.accessibilityValue = "closureAlert"
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
             action in
                 ref.dismiss(animated: true, completion: nil)
